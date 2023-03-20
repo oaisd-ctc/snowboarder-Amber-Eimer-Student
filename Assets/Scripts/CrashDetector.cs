c4621dba00f5e0afd3564e6be758bd3528a7a4c5
Amber-Eimer-Student <112406480+Amber-Eimer-Student@users.noreply.github.com>
2023-03-20 09:23:55 -0400

    Delay

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
-    [SerializeField] float loadDelay=1f;
+    [SerializeField] float loadDelay = 0.5f;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Ground")
