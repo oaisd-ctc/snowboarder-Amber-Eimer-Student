36dbdcb0b8e0050d03b32671ae1228657952ecc8
Amber-Eimer-Student <112406480+Amber-Eimer-Student@users.noreply.github.com>
2023-03-21 10:14:40 -0400

    DustTrail added

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -7,7 +7,7 @@ public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float loadDelay = 0.5f;
     [SerializeField] ParticleSystem crashEffect;
-    private void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Ground")
         {
